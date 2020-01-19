b9c5e5dc454907baec4448b51cbffe3bd86c13da
Rafael Costa <rafaelsevla@gmail.com>
2020-01-19 13:36:26 -0300

    finish frontend

--- Week10/frontend/src/App.js
+++ Week10/frontend/src/App.js
@@ -1,172 +1,43 @@
 import React, { useState, useEffect } from 'react'
+import api from './services/api'
 
 import './global.css'
 import './App.css'
 import './Sidebar.css'
 import './Main.css'
 
+import DevItem from './ui/DevItem'
+import DevForm from './ui/DevForm'
+
 function App() {
-  const [github_username, setGithubUsername] = useState('')
-  const [techs, setTechs] = useState('')
-  const [latitude, setLatitude] = useState('')
-  const [longitude, setLongitude] = useState('')
+  const [devs, setDevs] = useState([])
 
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(
-      position => {
-        const { latitude, longitude } = position.coords
-
-        setLatitude(latitude)
-        setLongitude(longitude)
-      },
-      err => {
-        console.log(err)
-      },
-      {
-        timeout: 30000
-      }
-    )
+    async function loadDevs() {
+      const response = await api.get('/devs')
+      setDevs(response.data)
+    }
+    loadDevs()
   }, [])
+
+  async function handleAddDev(data) {
+    const response = await api.post('/devs', data)
+
+    setDevs([...devs, response.data])
+  }
 
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form>
-          <div className="input-block">
-            <label htmlFor="github_username">Usuário do Github</label>
-            <input
-              name="github_username"
-              id="github_username"
-              required
-              value={github_username}
-              onChange={e => setGithubUsername(e.target.value)}
-            />
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="techs">Tecnologias</label>
-            <input
-              name="techs"
-              id="techs"
-              required
-              value={techs}
-              onChange={e => setTechs(e.target.value)}
-            />
-          </div>
-
-          <div className="input-group">
-            <div className="input-block">
-              <label htmlFor="latitude">Latitude</label>
-              <input
-                type="number"
-                name="latitude"
-                id="latitude"
-                required
-                value={latitude}
-                onChange={e => setLatitude(e.target.value)}
-              />
-            </div>
-
-            <div className="input-block">
-              <label htmlFor="longitude">Longitude</label>
-              <input
-                type="number"
-                name="longitude"
-                id="longitude"
-                required
-                value={longitude}
-                onChange={e => setLongitude(e.target.value)}
-              />
-            </div>
-          </div>
-
-          <button type="submit">Salvar</button>
-        </form>
+        <DevForm onSubmit={handleAddDev} />
       </aside>
 
       <main>
         <ul>
-          <li className="dev-item">
-            <header>
-              <img
-                src="https://avatars3.githubusercontent.com/u/38443896?s=460&v=4"
-                alt="Rafaelcosta"
-              />
-              <div className="user-info">
-                <strong>Rafael Costa</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-            <p>Uncaught ReferenceError: bio is not defined</p>
-            <a href="https://github.com/rafaelsevla">
-              Acessar perfil no Github
-            </a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img
-                src="https://avatars3.githubusercontent.com/u/38443896?s=460&v=4"
-                alt="Rafaelcosta"
-              />
-              <div className="user-info">
-                <strong>Rafael Costa</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-            <p>Uncaught ReferenceError: bio is not defined</p>
-            <a href="https://github.com/rafaelsevla">
-              Acessar perfil no Github
-            </a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img
-                src="https://avatars3.githubusercontent.com/u/38443896?s=460&v=4"
-                alt="Rafaelcosta"
-              />
-              <div className="user-info">
-                <strong>Rafael Costa</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-            <p>Uncaught ReferenceError: bio is not defined</p>
-            <a href="https://github.com/rafaelsevla">
-              Acessar perfil no Github
-            </a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img
-                src="https://avatars3.githubusercontent.com/u/38443896?s=460&v=4"
-                alt="Rafaelcosta"
-              />
-              <div className="user-info">
-                <strong>Rafael Costa</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-            <p>Uncaught ReferenceError: bio is not defined</p>
-            <a href="https://github.com/rafaelsevla">
-              Acessar perfil no Github
-            </a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img
-                src="https://avatars3.githubusercontent.com/u/38443896?s=460&v=4"
-                alt="Rafaelcosta"
-              />
-              <div className="user-info">
-                <strong>Rafael Costa</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-            <p>Uncaught ReferenceError: bio is not defined</p>
-            <a href="https://github.com/rafaelsevla">
-              Acessar perfil no Github
-            </a>
-          </li>
+          {devs.map(dev => (
+            <DevItem key={dev._id} dev={dev} />
+          ))}
         </ul>
       </main>
     </div>
